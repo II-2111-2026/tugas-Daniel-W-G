bc4e7a09312a14bbec4125d8121513049cf89c48
DaWG <danielwicaksanag@gmail.com>
2026-03-15 22:59:23 +0700

    Revert "Tugas Daniel 18224114"
    This reverts commit c2eea14aacee4577c1a86aa95c5691b06fa11085.

--- submissions/w01/answers.py
+++ submissions/w01/answers.py
@@ -13,17 +13,17 @@ from __future__ import annotations
 def q01() -> bool:
     """[T/F] Dalam model probabilistik, output yang sama akan selalu dihasilkan dari input yang
 sama terlepas dari variasi acak."""
-    raise False
+    raise NotImplementedError
 
 def q02() -> bool:
     """[T/F] Ruang sampel dari sebuah eksperimen acak harus mencakup semua hasil yang
 mungkin terjadi tanpa tumpang tindih."""
-    raise True
+    raise NotImplementedError
 
 def q03() -> bool:
     """[T/F] Probabilitas empiris mendekati probabilitas teoretis ketika jumlah percobaan
 mendekati tak hingga."""
-    raise True
+    raise NotImplementedError
 
 def q04() -> str:
     """[MC] Manakah yang merupakan contoh variabel acak dalam sistem STI?
@@ -32,7 +32,7 @@ A) Kapasitas total hard disk 1TB.
 B) Jumlah core pada prosesor Intel i7.
 C) Waktu yang dibutuhkan untuk merespons query database.
 D) Jumlah bit dalam satu byte."""
-    raise "C"
+    raise NotImplementedError
 
 def q05() -> str:
     """[MC] Jika sebuah ruang sampel S terdiri dari 4 kejadian yang memiliki peluang sama,
@@ -42,7 +42,7 @@ A) 0,5
 B) 0,25
 C) 0,75
 D) 1,0"""
-    raise "B"
+    raise NotImplementedError
 
 def q06() -> str:
     """[MC] Kejadian yang mustahil terjadi memiliki nilai probabilitas sebesar:
@@ -51,7 +51,7 @@ A) 0
 B) 1
 C) -1
 D) 0,5"""
-    raise "A"
+    raise NotImplementedError
 
 def q07() -> str:
     """[MC] Sekumpulan hasil eksperimen yang merupakan subset dari ruang sampel disebut:
@@ -60,29 +60,29 @@ A) Populasi.
 B) Parameter.
 C) Kejadian (Event).
 D) Konstanta."""
-    raise "C"
+    raise NotImplementedError
 
 def q08() -> float:
     """[Numeric] Berapa jumlah elemen dalam ruang sampel jika kita melempar dua buah
 dadu bersisi enam?"""
-    raise 36
+    raise NotImplementedError
 
 def q09() -> float:
     """[Numeric] Jika probabilitas sebuah link internet mati adalah 0,01, berapa probabilitas
 link tersebut hidup?"""
-    raise 0.99
+    raise NotImplementedError
 
 def q10() -> float:
     """[Numeric] Berapa banyak susunan berbeda yang bisa dibuat dari kata "DATA"?"""
-    raise 12
+    raise NotImplementedError
 
 def q11() -> float:
     """[Numeric] Jika sebuah server memiliki probabilitas uptime 0,95, berapa probabilitas
 server tersebut downtime dalam satu periode?"""
-    raise 0.05
+    raise NotImplementedError
 
 def q12() -> float:
     """[Numeric] Dalam simulasi 1000 kali login, jika 20 kali gagal, berapa frekuensi relatif
 kegagalan tersebut?"""
-    raise 0.02
+    raise NotImplementedError
 
